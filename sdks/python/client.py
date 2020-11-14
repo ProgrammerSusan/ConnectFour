1e471fc42f704f7c7499aeb64416773323562716
wxc366 <53020518+wxc366@users.noreply.github.com>
2020-11-14 16:51:13 -0500

    Update client.py
    Added the validMove function

--- sdks/python/client.py
+++ sdks/python/client.py
@@ -4,36 +4,45 @@ import sys
 import json
 import socket
 
+
 def get_move(player, board):
-  # TODO determine valid moves
-  # TODO determine best move
-  return {"column": 1}
+    # TODO determine valid moves
+    # TODO determine best move
+    return {"column": 1}
+
+
+def validMove(column, board):
+    if board[0][column] != 0:
+        return 0
+
 
 def prepare_response(move):
-  response = '{}\n'.format(json.dumps(move))
-  print('sending {!r}'.format(response))
-  return response
+    response = '{}\n'.format(json.dumps(move))
+    print('sending {!r}'.format(response))
+    return response
+
 
 if __name__ == "__main__":
-  port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1337
-  host = sys.argv[2] if (len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname()
+    port = int(sys.argv[1]) if (len(sys.argv) > 1 and sys.argv[1]) else 1338
+    host = sys.argv[2] if (
+        len(sys.argv) > 2 and sys.argv[2]) else socket.gethostname()
 
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  try:
-    sock.connect((host, port))
-    while True:
-      data = sock.recv(1024)
-      if not data:
-        print('connection to server closed')
-        break
-      json_data = json.loads(str(data.decode('UTF-8')))
-      board = json_data['board']
-      maxTurnTime = json_data['maxTurnTime']
-      player = json_data['player']
-      print(player, maxTurnTime, board)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((host, port))
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                print('connection to server closed')
+                break
+            json_data = json.loads(str(data.decode('UTF-8')))
+            board = json_data['board']
+            maxTurnTime = json_data['maxTurnTime']
+            player = json_data['player']
+            print(player, maxTurnTime, board)
 
-      move = get_move(player, board)
-      response = prepare_response(move)
-      sock.sendall(response)
-  finally:
-    sock.close()
+            move = get_move(player, board)
+            response = prepare_response(move)
+            sock.sendall(response)
+    finally:
+        sock.close()
